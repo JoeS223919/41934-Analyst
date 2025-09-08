19fbe8df6a8251031c8f7f506c19341b42c1d862
JoeS223919 <s223919@student.dtu.dk>
2025-09-08 17:05:46 +0200

    Undid some things and added .ifc file to data folder
    The ifc file is now in a folder in the repo and can be run by all

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from rules import windowRule
 from rules import doorRule
 from rules import beamRule
 
-model = ifcopenshell.open("/Users/johannes.a.f/Library/CloudStorage/OneDrive-Personal/DTU/Kandidat/7. semester/Advanced BIM/Structure/25-16-D-STR.ifc")
+model = ifcopenshell.open("data/25-16-D-STR.ifc")
 
 windowResult = windowRule.checkRule(model)
 doorResult = doorRule.checkRule(model)
