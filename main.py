2b36c6eeac5c63ce79054ac0243202ccc09253a0
JoeS223919 <s223919@student.dtu.dk>
2025-09-08 16:27:42 +0200

    beamRule
    I have added a new rule called beamRule its very cool

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import ifcopenshell
 
 from rules import windowRule
 from rules import doorRule
+from rules import beamRule
 
-model = ifcopenshell.open("path/to/ifcfile.ifc")
+model = ifcopenshell.open("/Users/johannes.a.f/Library/CloudStorage/OneDrive-Personal/DTU/Kandidat/7. semester/Advanced BIM/Structure/25-16-D-STR.ifc")
 
 windowResult = windowRule.checkRule(model)
 doorResult = doorRule.checkRule(model)
+beamResult = beamRule.checkRule(model)
 
 print("Window result:", windowResult)
 print("Door result:", doorResult)
+print("Beam result:", beamResult)
+
---
+++ rules/beamRule.py
@@ -0,0 +1,8 @@
+import ifcopenshell
+
+def checkRule(model):
+    beams = model.by_type('IfcBeam')
+
+    result = f"Beams: {len(beams)}"
+
+    return result
