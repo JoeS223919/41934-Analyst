01000042d3fe0328a3eb67eaf050dbe82404008c
JoeS223919 <s223919@student.dtu.dk>
2025-09-25 16:46:20 +0200

    updated main to add a print for Dimensions

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ from rules import beamRule
 from rules import BeamClassifications
 from rules import checkClassification
 from rules import checkBeamClassification
-from rules import beamDimensions
 from rules import Dimensions
-from Dimensions import beam_dimensions
+
 
 model = ifcopenshell.open("samples/25-16-D-STR.ifc")
 model1 = ifcopenshell.open("samples/Exercise9_Group10.ifc")
@@ -22,17 +21,19 @@ beamResult = beamRule.checkRule(model)
 # beamClassificationsResult = BeamClassifications.checkRule(model1, 1)
 
 
+DimensionsResult = Dimensions.beam_dimensions(model)
+
+
 
 # print("Window result:", windowResult)
 # print("Door result:", doorResult)
-print("Beam result:", beamResult)
 # print("Beam classifications:", beamClassificationsResult)
 # print('--------------------------------------------------')
 # print('Number of beams with classification: ', len(checkBeamClassificationResult[0]), 
 #       'Number of beams without classification: ', len(checkBeamClassificationResult[1]),
 #       'Total number of beams: ', len(model1.by_type("IfcBeam")))
 
-for beam_info in beam_dimensions(model):
+for beam_info in DimensionsResult:
     print(f"GlobalId: {beam_info['GlobalId']}, Name: {beam_info['Name']}")
     print(f"  Dimensions: {beam_info['Dimensions']}")
     print(f"  Cut Length: {beam_info['CutLength']}")
